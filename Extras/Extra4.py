d2df4f71ff881b8faeb73cff0d4590ed00a806f6
DavidHortelano-ITB2425 <david.hortelano.7e8@itb.cat>
2024-09-27 09:42:05 +0200

    HelloWorld.py ComprovarEdat.py

---
+++ Extras/Extra4.py
@@ -0,0 +1,13 @@
+# Assignar valors a les variables
+juan = 3
+maria = 5
+adan = 6
+
+# Imprimir les variables en una línia separades per comes
+print(juan, maria, adan, sep=',')
+
+# Calcular el total de pomes
+total_pomes = juan + maria + adan
+
+# Imprimir el total de pomes
+print(total_pomes)
