a56ca6d57d2e966a5500bc8107f0eea0bc2f43cc
david.hortelano.7e8 <david.hortelano.7e8@itb.cat>
2024-10-10 10:29:39 +0200

    ej_1 ej_2

---
+++ Ejercicios de prueba/ej_1.py
@@ -0,0 +1,5 @@
+num1 = int(input("Dime un numero"))
+num2 = int(input("Dime otro numero"))
+resultado = num1 + num2
+
+print(resultado)
---
+++ Ejercicios de prueba/ej_2.py
@@ -0,0 +1,14 @@
+num1 = int(input("Dime un numero: "))
+num2 = int(input("Dime otro: "))
+
+num1_float = float(num1)
+num2_float = float(num2)
+
+suma_floats = num1_float + num2_float
+
+suma_entera = int(suma_floats)
+
+print(f"El primer numero float: {num1_float}")
+print(f"El segondo numero flot: {num2_float}")
+print(f"La suma: {suma_floats}")
+print(f"La suma convertida a entero: {suma_entera}")
